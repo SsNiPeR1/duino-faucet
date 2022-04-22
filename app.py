2f99c70deade47eada8f16c77d09bd3a2d94796e
SsNiPeR1 <denis.mitin.005@gmail.com>
2022-04-22 11:11:21 +0200

    added simple captcha, will make captcha 2.0 soon.

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, request
+import numbers
+from flask import Flask, render_template, request, send_file
 import json
 import os
+from numpy import number
 import requests
+from captcha.image import ImageCaptcha
+import random
 
 app = Flask(__name__)
 
@@ -29,15 +33,34 @@ else:
 def css():
     return open("static/style.css", "r").read()
 
+@app.route("/captcha")
+def cpth():
+    numbers = random.randint(1, 1000000)
+    global validcaptchas
+    validcaptchas = []
+    validcaptchas.append(str(numbers))
+    image = ImageCaptcha(fonts=["static/arial.ttf"])
+    data = image.generate(str(numbers))
+    return send_file(data, mimetype="image/png")
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        data = requests.get(f"https://server.duinocoin.com/transaction?username={user}&password={password}&recipient={request.form.get('textbox')}&amount=0.01&memo=Free%20coins%20from%20faucet").text
-        retdata = str(json.loads(data)['success'])
-        if "True" in retdata:
-            return render_template("success.html", sent=request.form.get('textbox'))
-        else:
-            return render_template("failure.html", sent=request.form.get('textbox'))
+        try:
+            if str(request.form.get('captcha')) in validcaptchas:
+                data = requests.get(f"https://server.duinocoin.com/transaction?username={user}&password={password}&recipient={request.form.get('textbox')}&amount=0.01&memo=Free%20coins%20from%20faucet").text
+                retdata = str(json.loads(data)['success'])
+                if "True" in retdata:
+                    validcaptchas.remove(request.form.get('captcha'))
+                    return render_template("success.html", sent=request.form.get('textbox'))
+                else:
+                    validcaptchas.remove(request.form.get('captcha'))
+                    return render_template("failure.html", sent=request.form.get('textbox'))
+
+            else:
+                return "Invalid token."
+        except:
+            return "Invalid token."
 
     jsondata = requests.get(f"https://server.duinocoin.com/balances/{user}").text
     jsonobj = json.loads(jsondata)
